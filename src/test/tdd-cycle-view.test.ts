df1e83119bb760ccdfa60cca292a5353bcae494c
andrea.colombo45 <andrea.colombo45@studenti.unimi.it>
2025-07-04 15:56:16 +0200

    GREEN: Generates correctly HTML for different phases

--- src/test/tdd-cycle-view.test.ts
+++ src/test/tdd-cycle-view.test.ts
@@ -2,16 +2,21 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 import * as sinon from 'sinon';
 import { TddCycleView } from '../views/tdd-cycle-view';
+import { TddStateManager } from '../services/tdd-state-manager';
+import { TddPhase } from '../models/tdd-models';
 
 suite('TddCycleView Test Suite', () => {
     let tddCycleView: TddCycleView;
     let mockExtensionUri: vscode.Uri;
     let mockWebview: vscode.Webview;
     let mockWebviewView: vscode.WebviewView;
+    let stateManager: TddStateManager;
 
     setup(() => {
+        (TddStateManager as any).instance = undefined;
+        stateManager = TddStateManager.getInstance();
+
         mockExtensionUri = vscode.Uri.file('/mock/extension/path');
-        tddCycleView = new TddCycleView(mockExtensionUri);
 
         mockWebview = {
             html: '',
@@ -31,6 +36,12 @@ suite('TddCycleView Test Suite', () => {
             title: 'Test View',
             description: 'Test Description'
         } as any;
+
+        tddCycleView = new TddCycleView(mockExtensionUri);
+    });
+
+    teardown(() => {
+        sinon.restore();
     });
 
     test('Should initialize with correct view type', () => {
@@ -47,5 +58,23 @@ suite('TddCycleView Test Suite', () => {
         assert.strictEqual(mockWebview.options.enableScripts, true);
         assert.ok(mockWebview.options.localResourceRoots);
         assert.ok(mockWebview.html.length > 0);
+    });
+
+    test("Should generete correct HTML content for different phases", () => {
+        const context = {} as vscode.WebviewViewResolveContext;
+        const token = {} as vscode.CancellationToken;
+
+        stateManager.setPhase(TddPhase.PICK);
+        tddCycleView.resolveWebviewView(mockWebviewView, context, token);
+        assert.ok(mockWebview.html.includes('PICK'));
+        
+        stateManager.setPhase(TddPhase.RED);
+        assert.ok(mockWebview.html.includes('RED'));
+
+        stateManager.setPhase(TddPhase.GREEN);
+        assert.ok(mockWebview.html.includes('GREEN'));
+
+        stateManager.setPhase(TddPhase.REFACTORING);
+        assert.ok(mockWebview.html.includes('REFACTORING'));
     });
 });
