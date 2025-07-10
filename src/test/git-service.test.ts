2adefa9089b6d5039e052771b3ccc62cc4020e9e
andrea.colombo45 <andrea.colombo45@studenti.unimi.it>
2025-07-10 18:25:45 +0200

    GREEN: Creates GitService if inside a Git repo

---
+++ src/test/git-service.test.ts
@@ -0,0 +1,31 @@
+import * as assert from 'assert';
+import * as sinon from 'sinon';
+import * as vscode from 'vscode';
+import { GitService } from '../services/git-service';
+
+suite('GitService Test Suite', () => {
+    let execStub: sinon.SinonStub;
+    let workspaceFolderStub: sinon.SinonStub;
+
+    setup(() => {
+        const workspaceFolder = {
+            uri: { fsPath: '/mock/workspace' },
+            name: 'mock-workspace',
+            index: 0
+        };
+        workspaceFolderStub = sinon.stub(vscode.workspace, 'workspaceFolders').value([workspaceFolder]);
+    });
+
+    teardown(() => {
+        sinon.restore();
+    });
+
+    test('Should create GitService instance', async () => {
+        execStub = sinon.stub().resolves({ stdout: 'true' });
+
+        const gitService = await GitService.create(execStub);
+
+        assert.ok(gitService instanceof GitService);
+        assert.notStrictEqual(gitService, null);
+    });
+});
